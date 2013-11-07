6db1247f816532b3539af48d3b13406db5d63d77
Vladislav Brylinskiy <vbrilinskiy@gmail.com>
2013-11-07 22:10:03 +0200

    Initial Commit

---
+++ GKSessionWrappers/GKSessionWrappers.h
@@ -0,0 +1,13 @@
+//
+//  GKSessionWrappers.h
+//  GKSessionWrappers
+//
+//  Created by Vladislav Brylinskiy on 07.11.13.
+//  Copyright (c) 2013 Vladislav Brylinskiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GKSessionWrappers : NSObject
+
+@end
---
+++ GKSessionWrappers/GKSessionWrappers.m
@@ -0,0 +1,13 @@
+//
+//  GKSessionWrappers.m
+//  GKSessionWrappers
+//
+//  Created by Vladislav Brylinskiy on 07.11.13.
+//  Copyright (c) 2013 Vladislav Brylinskiy. All rights reserved.
+//
+
+#import "GKSessionWrappers.h"
+
+@implementation GKSessionWrappers
+
+@end
